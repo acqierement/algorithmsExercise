75fa4081e19179f338cad678d6c5d4a253323493
acqierement <337048545@qq.com>
2018-10-09 20:35:28 +0800

    add ListExcise

--- algorithms/src/list/Palindrome_linked_list_234.java
+++ algorithms/src/list/Palindrome_linked_list_234.java
@@ -1,53 +1,63 @@
 package list;
 
 public class Palindrome_linked_list_234 {
-	public static void main(String[] args) {
-		ListNode listNode = new ListNode(1);
-		ListNode se =new ListNode(1);
-		
-		listNode.next =se;
-		
-		listNode.next.next = new ListNode(2);
-		listNode.next.next.next = new ListNode(1);
-		listNode.next.next.next.next = new ListNode(1);
-		listNode.next.next.next.next.next = null;
-		boolean palindrome = isPalindrome(listNode);
-		System.out.println(palindrome);
-
-
-	}
-	
-	public static boolean isPalindrome(ListNode head) {
+	public  boolean isPalindromeByReverse(ListNode head) {
         boolean result = true;
         if(head == null) return result;
         ListNode cur = copyListNode(head);
         ListNode reverseList = reverse(head);
-       
+        
         while(cur!=null){
             if(cur.val != reverseList.val){result = false;break;}
             else{
-            	System.out.println("cur.val: "+cur.val);
-            	System.out.println("reverse.val: "+reverseList.val);
             	cur = cur.next;
                  reverseList = reverseList.next;}
         }
-        
         return result ;
-        
     }
-	public static ListNode copyListNode(ListNode head) {
+	
+	public boolean isPalindromeByFindCenter(ListNode head) {
+		boolean result = true;
+        if(head == null) return result;
+		ListNode center = findCenter(head);
+		ListNode reverse = reverse(center);
+		while(reverse != null) {
+			if (reverse.val != head.val) {
+				result = false;
+				break;
+			}else {
+				reverse = reverse.next;
+				head = head.next;
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	public ListNode findCenter(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while(fast!=null&&fast.next !=null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}	
+	
+	public  ListNode copyListNode(ListNode head) {
         ListNode copy = new ListNode(0);
         ListNode cur =copy;
       
         while(head!=null){
             cur.next = new ListNode(head.val);
             cur = cur.next;
-         //   System.out.println(cur.val);
             head = head.next;
         }
         return copy.next;
 	}
-    public static ListNode reverse(ListNode head){
+	
+    public  ListNode reverse(ListNode head){
         ListNode prev = null;
         while(head!= null){
             ListNode temp = head.next;

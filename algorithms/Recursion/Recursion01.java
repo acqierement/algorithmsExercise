fecc724d928b40527fdcc902cb82c6a9f4fffe68
acqierement <337048545@qq.com>
2019-02-26 10:33:29 +0800

    add Recursion

---
+++ algorithms/Recursion/Recursion01.java
@@ -0,0 +1,13 @@
+public class Recursion01 {
+    public static int fibonacci_2(int n) {
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibonacci_2(n - 1) + fibonacci_2(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci_2(5));//5
+    }
+}
---
+++ algorithms/Recursion/readme.md
@@ -0,0 +1,5 @@
+1. 编程实现斐波那契数列求值 f(n)=f(n-1)+f(n-2);其中f(0)=0,f(1)=1
+
+2. 实现阶乘
+
+3. 返回一组数据的全排列 LeetCode[46. Permutations](https://leetcode.com/problems/permutations/)

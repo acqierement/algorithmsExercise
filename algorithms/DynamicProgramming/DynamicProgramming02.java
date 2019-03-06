a78ad373eb4f5299b7371c70c5672f5352e0d6fb
acqierement <337048545@qq.com>
2019-03-06 15:51:03 +0800

    add DynamicProgramming

---
+++ algorithms/DynamicProgramming/DynamicProgramming02.java
@@ -0,0 +1,16 @@
+/**
+ * 打家劫舍
+ */
+public class DynamicProgramming02 {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        sum[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            sum[i] = Math.max(nums[i - 1] + sum[i - 2], sum[i - 1]);
+        }
+        return sum[nums.length];
+    }
+}
---
+++ algorithms/DynamicProgramming/readme.md
@@ -0,0 +1,4 @@
+## 动态规划相关题目
+ 
+ 1. 打家劫舍 LeetCode[198. House Robber](https://leetcode.com/problems/house-robber/)
+    这题在LeetCode上标记为简单的题目，结果一开始我却无从下手，其实这是一道最简单的动态规划的题目
